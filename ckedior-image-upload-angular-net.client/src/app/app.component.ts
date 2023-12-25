dec13e0c04d72f9af2fe399966f9e6e77ef7cb1a
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2023-12-26 00:52:10 +0200

    App component cleanup

--- ckedior-image-upload-angular-net.client/src/app/app.component.ts
+++ ckedior-image-upload-angular-net.client/src/app/app.component.ts
@@ -1,24 +1,15 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import Editor from 'ckeditor5/build/ckeditor';
-
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  public forecasts: WeatherForecast[] = [];
-
-  constructor(private http: HttpClient) {}
+export class AppComponent {
+  title = 'ckedior-image-upload-angular-net.client';
+  constructor() {}
 
   public editor = Editor;
 
@@ -27,22 +18,5 @@ export class AppComponent implements OnInit {
     body: new FormControl('', Validators.required),
   });
 
-  ngOnInit() {
-    // this.getForecasts();
-  }
-
   onSubmit() {}
-
-  // getForecasts() {
-  //   this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
-  //     (result) => {
-  //       this.forecasts = result;
-  //     },
-  //     (error) => {
-  //       console.error(error);
-  //     }
-  //   );
-  // }
-
-  title = 'ckedior-image-upload-angular-net.client';
 }
